c2bf0023bb0d8da6aec27a4f91f3023600eaf8d4
mippado <60156870+mippado@users.noreply.github.com>
2021-03-08 23:43:42 -0700

    Upload Assignment 9
    drag and drop assignment 9 folder

---
+++ HW 9/sketch.js
@@ -0,0 +1,32 @@
+function setup() {
+  createCanvas(500, 600);
+}
+
+function draw() {
+  background('orange');
+  fill('tan');
+  circle(250, 160, 75);
+  line(250, 200, 250, 325);
+  fill('red');
+  rect(245,240,10,75)
+  fill('blue');
+  triangle(249, 310, 200, 355, 247, 355);
+   triangle(251, 310, 251, 355, 297, 355);
+  line(250,325,300,400);
+  line(250,325,200, 400);
+  line(250,265, 300, 200 );
+  line(250, 265, 200, 200);
+  fill('white');
+  triangle(235, 145, 240, 155, 230, 155);
+  triangle(265, 145, 260, 155, 270, 155);
+  circle(250,175,25);
+  fill('green');
+  rect(1,399,499,199);
+  point(90, 75);
+  fill('yellow')
+  circle(90,75,100)
+  fill('black')
+  text('Warm Summer Day', 192, 50);
+  text('Michael Matthews', 202, 500)
+
+}
