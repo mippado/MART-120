04844b67cf8f4154535a93f5c3846bd0b0af4232
mippado <60156870+mippado@users.noreply.github.com>
2021-04-14 10:52:01 -0600

    upload final files
    upload final files

---
+++ final game/script.js
@@ -0,0 +1,27 @@
+var score = 0;
+
+var character = document.getElementById("character");
+
+var block = document.getElementById("block");
+
+function jump(){
+  if(character.classList != "animate"){
+  character.classList.add("animate");}
+  setTimeout(function(){character.classList.remove("animate");},500);
+  score = score + 1;
+}
+
+var checkDead = setInterval(function(){
+  var charactertop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+  var blockleft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+  if(blockleft<20 && blockleft>0 && charactertop>=130){
+    block.style.animation = "none";
+    block.style.display = "none";
+    alert("You lose!");}
+
+
+function draw() {
+  text("Score: " + score,20,20);
+}
+
+},10);
